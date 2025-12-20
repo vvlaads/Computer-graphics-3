8279da3852711b54087a3eb8302247ea427161a6
vvlaads <vvlaads@gmail.com>
2025-12-20 12:03:23 +0300

    Fixed units of measurement

--- src/illumination.py
+++ src/illumination.py
@@ -1,16 +1,38 @@
 import numpy as np
+import math
 
 
 def get_illumination(power, px_mm, py_mm, sx_mm, sy_mm, sz_mm):
     """Формула подсчета освещенности в точке на плоскости z = 0 от точечного источника света"""
-    diff_x = abs(px_mm - sx_mm) / 1000
-    diff_y = abs(py_mm - sy_mm) / 1000
-    diff_z = sz_mm / 1000
+    # Переводим миллиметры в метры (делим на 1000)
+    px_m = px_mm / 1000.0
+    py_m = py_mm / 1000.0
+    sx_m = sx_mm / 1000.0
+    sy_m = sy_mm / 1000.0
+    sz_m = sz_mm / 1000.0
 
-    distance = diff_x ** 2 + diff_y ** 2 + diff_z ** 2
-    cos = diff_z / distance
+    # Разницы координат в метрах
+    diff_x = px_m - sx_m
+    diff_y = py_m - sy_m
+    diff_z = 0 - sz_m  # плоскость z = 0
 
-    return power * cos / (distance ** 2)
+    # Квадрат расстояния в метрах
+    distance_squared = diff_x ** 2 + diff_y ** 2 + diff_z ** 2
+
+    if distance_squared == 0:
+        return 0  # Чтобы избежать деления на 0
+
+    # Расстояние
+    distance = math.sqrt(distance_squared)
+
+    # Косинус угла между нормалью к плоскости (0,0,1) и направлением на источник
+    # cos(θ) = |sz_m| / distance (нормаль направлена вверх, источник выше плоскости)
+    cos_theta = abs(sz_m) / distance
+
+    # Освещенность E = I * cos(θ) / r²
+    illumination = power * cos_theta / distance_squared
+
+    return illumination
 
 
 def grid_centers(width_mm, height_mm, w_res, h_res):
